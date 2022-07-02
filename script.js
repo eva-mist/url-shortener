19e3970e93a455f2be527e1d94e0ec8a386cc780
eva-mist <66295847+eva-mist@users.noreply.github.com>
2022-07-02 15:57:39 +0100

    added modal

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementById("close");
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function myFunction() {
+    var copyText = document.getElementById("myURL");
+
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+
+    navigator.clipboard.writeText(copyText.value);
+
+    alert("Copied to Clipboard");
+}
